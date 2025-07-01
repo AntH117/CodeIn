c67d5c2545f6eedc214438af2ff5e239ad86e9d9
Anthony <anthony11715@hotmail.com>
2025-07-01 11:56:10 +1000

    .

--- client/src/index.js
+++ client/src/index.js
@@ -73,7 +73,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} basename={basename}/>
+      <RouterProvider router={router} basename="/CodeIn" />
     </AuthProvider>
   </React.StrictMode>
 );
