20a768b2743fb9f9dbc35dbf28c5c01f80c49ec8
Anthony <anthony11715@hotmail.com>
2025-07-07 11:06:52 +1000

    s

--- client/src/index.js
+++ client/src/index.js
@@ -67,13 +67,16 @@ const router = createBrowserRouter([
       }
     ]
   },
-]);
+], {
+  basename: basename
+}
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <RouterProvider router={router}/>
     </AuthProvider>
   </React.StrictMode>
 );
